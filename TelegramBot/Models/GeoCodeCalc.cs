7f48e2c6a793402c72b454b391103b434f2dba32
sobhan <sobhan.plus.plus@gmail.com>
2017-11-28 11:47:06 -0800

    fdsfsd

--- TelegramBot/Models/GeoCodeCalc.cs
+++ TelegramBot/Models/GeoCodeCalc.cs
@@ -21,6 +21,8 @@ namespace TelegramBot.Models
         //sdfsdfs
         //sdfbsdfhjsd
         //dsjhfkdjs
+        //sdjgfsdgjfgds
+        //ksdhfhsdgj
         public static double CalcDistance(double lat1, double lng1, double lat2, double lng2, GeoCodeCalcMeasurement m)
         {
             double radius = GeoCodeCalc.EarthRadiusInMiles;
